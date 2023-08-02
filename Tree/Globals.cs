7a5b211fe2ed29f0afa3370f7def8a1cde5a2372
Christof <christof_sprenger@hotmail.com>
2023-08-02 09:45:10 -0700

    remove spike

--- Tree/Globals.cs
+++
@@ -1,8 +0,0 @@
-
-global using System.Collections.ObjectModel;
-global using System.Diagnostics;
-global using System.Diagnostics.CodeAnalysis;
-global using System.Xml;
-global using System.Xml.Linq;
-
-global using static OData.Lite.XmlNamespaces;
--- Tree/XmlNamespaces.cs
+++
@@ -1,9 +0,0 @@
-
-namespace OData.Lite
-{
-    static class XmlNamespaces
-    {
-        public static XNamespace EDMX = "http://docs.oasis-open.org/odata/ns/edmx";
-        public static XNamespace EDM = "http://docs.oasis-open.org/odata/ns/edm";
-    }
-}
